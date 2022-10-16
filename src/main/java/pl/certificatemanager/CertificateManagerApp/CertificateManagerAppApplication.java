9e135bbb221d86ae77e49a7b67c9eabfb0f2f8d2
saszotek <danielzielinski789@gmail.com>
2022-10-16 12:55:43 +0200

    initial commit

---
+++ src/main/java/pl/certificatemanager/CertificateManagerApp/CertificateManagerAppApplication.java
@@ -0,0 +1,13 @@
+package pl.certificatemanager.CertificateManagerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificateManagerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificateManagerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/certificatemanager/CertificateManagerApp/CertificateManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.certificatemanager.CertificateManagerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificateManagerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
